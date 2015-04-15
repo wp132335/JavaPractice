25dbd3a78c0095882ebbbdd79ded4cd21408e52d
최재혁 <@2335CJH>
2015-04-15 14:42:37 +0900

    .

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -21,9 +21,9 @@ public class CarTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Car2 car1 = new Car2("현대자동차", "제네시스", "검정색", 225, 50000000);
-		Car2 car2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
-		Car2 car3 = new Car2("삼성자동차", "SM7", "흰색", 200, 38000000);
+		Car3 car1 = new Car3("현대자동차", "제네시스", "검정색", 225, 50000000);
+		Car3 car2 = new Car3("기아자동차", "K7", "흰색", 246);
+		Car3 car3 = new Car3("삼성자동차", "SM7", "흰색");
 		
 		/*
 		Car car1 = new Car();
