1fb1c6efa856cc07acca69856e58145f4c93f839
최재혁 <@2335CJH>
2015-11-06 14:28:22 +0900

    .

--- JavaPractice/src/org/dimigo/thread/Race2.java
+++ JavaPractice/src/org/dimigo/thread/Race2.java
@@ -15,14 +15,14 @@ package org.dimigo.thread;
  * @author		: 최재혁
  * @version		: 1.0
  */
-public class Race {
+public class Race2 {
 
 	public static void main(String[] args) {
 		
 		System.out.println("main thread start");
 		
-		Thread t1 = new Runner("홍길동");
-		Thread t2 = new Runner("홍길순");
+		Thread t1 = new Thread(new Runner2("홍길동"));
+		Thread t2 = new Thread(new Runner2("홍길순"));
 		
 		t1.setPriority(Thread.MAX_PRIORITY);
 		t2.setPriority(Thread.MIN_PRIORITY);
